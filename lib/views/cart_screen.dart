507b56108fbbb0855abda84c662448d43cfe3b8f
Yuri Borges <49327925+YuriAres@users.noreply.github.com>
2024-02-05 13:04:01 -0300

    Update

--- lib/views/cart_screen.dart
+++ lib/views/cart_screen.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_foodstore/widgets/background_widget.dart';
+import 'package:flutter_foodstore/widgets/button_widget.dart';
 import 'package:flutter_foodstore/widgets/header2_widget.dart';
-import 'package:flutter_foodstore/widgets/header_widget.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class CartScreen extends StatefulWidget {
   const CartScreen({super.key});
@@ -13,12 +14,43 @@ class CartScreen extends StatefulWidget {
 class _CartScreenState extends State<CartScreen> {
   @override
   Widget build(BuildContext context) {
-    return const BackgroundContainer(
+    return BackgroundContainer(
         body: Column(
       children: [
-        Header2(
+        const Header2(
           title: "Cart",
         ),
+        SizedBox(
+          height: MediaQuery.of(context).size.height * 0.72,
+          width: MediaQuery.of(context).size.width,
+          child: ListView(
+            children: [
+              ListTile(
+                title: Text("Teste"),
+              )
+            ],
+          ),
+        ),
+        const Divider(),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text("Total price",
+                style: GoogleFonts.ubuntu(
+                    fontSize: MediaQuery.of(context).size.height * 0.028,
+                    fontWeight: FontWeight.w500,
+                    color: const Color(0xff4F623D))),
+            Text("\$ 15.00",
+                style: GoogleFonts.ubuntu(
+                    fontSize: MediaQuery.of(context).size.height * 0.028,
+                    fontWeight: FontWeight.w500,
+                    color: const Color(0xff4F623D)))
+          ],
+        ),
+        SizedBox(
+          height: MediaQuery.of(context).size.height * 0.018,
+        ),
+        CustomButton(textButton: "Payment")
       ],
     ));
   }
